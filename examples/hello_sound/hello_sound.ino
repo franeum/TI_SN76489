6afbbd447eefa0c113ed176275391132335d8c85
franeum <elettrochuck@gmail.com>
2023-12-12 22:57:26 +0100

    update

--- examples/hello_sound/hello_sound.ino
+++ examples/hello_sound/hello_sound.ino
@@ -1,26 +1,5 @@
-//#include <TI_SN76489.h>
+// #include <TI_SN76489.h>
 
-<<<<<<< HEAD
-//TI_SN76489 sn(9, 2,3,4,5,6,7,8,10,13);
-
-void setup() {
-  //sn.begin();
-  //sn.attenuation(0, 0);
-  //sn.frequency(0, 440);
-  Serial.begin(9600);
-  bool ahi = 3.1;
-
-  delay(3000);
-
-  if (ahi)
-    Serial.println("si");
-   else
-   Serial.println("no");
-}
-
-void loop() {
-  
-=======
 TI_SN76489 sn(2, 3, 4, 5, 6, 7, 8, 10, 13, 9, _4MHz);
 
 void setup()
@@ -46,5 +25,4 @@ void loop()
   delay(100);
   sn.frequency(0, 400);
   delay(100);
->>>>>>> c5e79ded62a6c441c255968bf603f502b9932c81
 }
